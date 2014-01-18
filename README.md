fc4319d9e37b0b17d9b155ca00b1090495665cab
Romain Gautier <mail@romain.sh>
2014-01-18 01:51:16 +0100

    added screen and readme

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-github-timeline
-===============
+# [Github Timeline](https://github.com/MyKiwi/github-timeline)
 
-Filter your Github timeline
+Filter your Github timeline by pull requests, issues, stars ...
+
+Exemple:  
+![Example with stars](screenshot/stars.png)
+
+---
+
+### Install for Firefox
+
+ * First step, you will need to download Greasemonkey from [here](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
+ * After you installed Greasemonkey and restarted Firefox, download the script from [here](https://github.com/downloads/MyKiwi/github-timeline/github-timeline.js).
+ * You will be promopted for the installation, after you click install you are done!
+ * Now you are ready to use it
+
+### Install for Opera
+
+ * Go to 'Preferences' (CTRL+F12) > Advanced > Content > Javascript Options
+ * Choose your 'User Javascript folder', somewhere nice
+ * Right click [here](https://github.com/downloads/MyKiwi/github-timeline/github-timeline.js) > 'Save Content Link As'
+ * Put it in your special nice javascript folder you just made and you are done. :)
+
+### Install for Chrome
+
+ * Press WinKey+R
+ * Type:
+```
+chrome --enable-easy-off-store-extension-install https://github.com/downloads/MyKiwi/github-timeline/github-timeline.js
+```
+* Press enter, confirm installation at the bottom
+
+
+### Install for Safari
+
+ * Install [NinjaKit](http://ss-o.net/safari/extension/NinjaKit.safariextz) and restart Safari
+ * Then click [here](https://github.com/downloads/MyKiwi/github-timeline/github-timeline.js)
+
+### Install for IE
+
+* Go [download Firefox](http://www.mozilla.org/en-US/firefox/is-better-than-ie)
+* Read Install for Firefox ;)
---
+++ github-timeline.js
@@ -0,0 +1,129 @@
+// ==UserScript==
+// @name        Github Timeline
+// @namespace   https://github.com/MyKiwi/github-timeline
+// @description Filter your github timeline
+// @include     https://github.com/
+// @version     1.0
+// @downloadURL https://github.com/MyKiwi/github-timeline
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+
+
+// Main class
+
+githubTimeline = {
+    css: {
+        dashboard: 'div.container div#dashboard div.news',
+        menu: {
+            base: 'div#github-timeline-menu',
+            child: 'a'
+        },
+
+        getMenu: function() {
+            'use strict';
+            return this.dashboard + ' ' + this.menu.base;
+        },
+
+        getItems: function() {
+            'use strict';
+            return this.getMenu() + ' ' + this.menu.child;
+        },
+
+        getItemSelected: function() {
+            'use strict';
+            return githubTimeline.css.getItems() + '.selected';
+        }
+    },
+
+    template: {
+        menu: '' +
+            '<div id="github-timeline-menu" class="issues-list-options">' +
+            '  <div class="button-group">' +
+            '    <a class="minibutton selected">All</a>' +
+            '    <a class="minibutton">Pull Requests</a>' +
+            '    <a class="minibutton">Issues</a>' +
+            '    <a class="minibutton">Stars</a>' +
+            '    <a class="minibutton">Others</a>' +
+            '  </div>' +
+            '</div>'
+    },
+
+    menu: {
+        select: function(child) {
+            'use strict';
+            $(githubTimeline.css.getItems() + '.selected').removeClass('selected');
+            child.addClass('selected');
+            githubTimeline.dashboard.refresh();
+        }
+    },
+
+    dashboard: {
+        refresh: function() {
+            'use strict';
+            var $button = $(githubTimeline.css.getItemSelected());
+
+            $dashboard.find('.alert').each(function(index, line) {
+                var $line = $(line);
+                $line.css('display', 'none');
+
+                var issueRegexp = new RegExp('issue', 'g');
+                var pullRequestRegexp = new RegExp('pull request', 'g');
+                switch ($button.html()) {
+                    case 'All':
+                        $line.css('display', 'block');
+
+                        break;
+                    case 'Pull Requests':
+                        if (pullRequestRegexp.test($line.find('div.title').html())) {
+                            $line.css('display', 'block');
+                        }
+
+                        break;
+                    case 'Issues':
+                        if (issueRegexp.test($line.find('div.title').html())) {
+                            $line.css('display', 'block');
+                        }
+
+                        break;
+                    case 'Stars':
+                        if ($line.hasClass('watch_started')) {
+                            $line.css('display', 'block');
+                        }
+
+                        break;
+                    case 'Others':
+                        if (!pullRequestRegexp.test($line.find('div.title').html()) && !issueRegexp.test($line.find('div.title').html()) && !$line.hasClass('watch_started')) {
+                            $line.css('display', 'block');
+                        }
+
+                        break;
+                }
+
+            });
+        }
+    }
+};
+
+
+// Workflow
+
+window.addEventListener('load', function() {
+
+    $dashboard = $(githubTimeline.css.dashboard);
+
+    $(githubTimeline.template.menu).prependTo($dashboard);
+    $menu = $(githubTimeline.css.getMenu());
+
+    $(document).on('click', githubTimeline.css.getItems(), function() {
+        githubTimeline.menu.select($(this));
+    });
+
+    $(document).ajaxComplete(function(event, request, options) {
+        var urlMore = /\/dashboard\/index\/\d+/g
+        if (urlMore.test(options.url)) {
+            githubTimeline.dashboard.refresh();
+        }
+    });
+
+});
